e2be40ee007b631b89a732d7465720bcb153e28d
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-03-01 17:09:03 +0100

    Fix - so many things

--- script/main.js
+++ script/main.js
@@ -2,76 +2,74 @@ import Maison from '../class/Maison.js';
 import Appartement from '../class/Appartement.js';
 import Terrain from '../class/Terrain.js';
 
-let ad_container = document.getElementById('container');
-let form = document.getElementById('form_ad');
-let new_ad;
 
-document.getElementById('type').addEventListener('change', function() {
-    let type = this.value;
-
-    // Hide all specific form elements
-    let elements = document.querySelectorAll('.form_element_specific');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.display = 'none';
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
     }
+}
 
-    // Display form elements specific to the selected type
-    let elementsToShow = document.querySelectorAll('.form_element_' + type);
-    for (let i = 0; i < elementsToShow.length; i++) {
-        elementsToShow[i].style.display = 'flex';
-    }
-});
+docReady(function() {
+    let form = document.getElementById('form_ad');
+    let new_ad;
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+    document.getElementById('type').addEventListener('change', function() {
+        let type = this.value;
 
-    // Variables for all types
-    let title = document.getElementById('title').value;
-    let type = document.getElementById('type').value;
-    let price = document.getElementById('price').value;
-    let location = document.getElementById('location').value;
-    let description = document.getElementById('description').value;
-    let surface = document.getElementById('surface').value;
+        // Hide all specific form elements
+        let elements = document.querySelectorAll('.form_element_specific');
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = 'none';
+        }
 
-    // House
-    let garden = document.getElementById('garden').value;
-    let box = document.getElementById('box').value;
-    let room = document.getElementById('room').value;
+        // Display form elements specific to the selected type
+        let elementsToShow = document.querySelectorAll('.form_element_' + type);
+        for (let i = 0; i < elementsToShow.length; i++) {
+            elementsToShow[i].style.display = 'flex';
+        }
+    });
 
-    // Apartment
-    let ground = document.getElementById('ground').value;
-    let elevator = document.getElementById('elevator').value;
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    // Land
-    let buildable = document.getElementById('buildable').value;
+        // Variables for all types
+        let title = document.getElementById('title').value;
+        let type = document.getElementById('type').value;
+        let price = document.getElementById('price').value;
+        let location = document.getElementById('location').value;
+        let description = document.getElementById('description').value;
+        let surface = document.getElementById('surface').value;
 
-    // Create new instance of appropriate child class based on selected type
-    if (type === 'maison') {
-        new_ad = new Maison(title, type, description, price, location, surface, garden, box, room);
-    } else if (type === 'appartement') {
-        new_ad = new Appartement(title, type, description, price, location, surface, ground, elevator, room, box);
-    } else if (type === 'terrain') {
-        new_ad = new Terrain(title, type, description, price, location, surface, buildable);
-    }
+        // House
+        let garden = document.getElementById('garden').value;
+        let box = document.getElementById('box').value;
+        let room = document.getElementById('room').value;
 
-    // Store new ad in localStorage
-    let ads = JSON.parse(localStorage.getItem('ads')) || [];
-    ads.push(new_ad);
-    localStorage.setItem('ads', JSON.stringify(ads));
+        // Apartment
+        let ground = document.getElementById('ground').value;
+        let elevator = document.getElementById('elevator').value;
 
-    console.log(ads);
+        // Land
+        let buildable = document.getElementById('buildable').value;
 
-    // update UI homepage to display new ad
-    ads.forEach(ad => {
-        let new_element = document.createElement('div');
-        new_element.className = 'ad';
-        new_element.innerHTML = `
-            <h2>${ad.title}</h2>
-            <p>${ad.price} €</p>
-            <p>${ad.location}</p>
-            <p>${ad.description}</p>
-        `;
-        ad_container.appendChild(new_element);
+        // Create new instance of appropriate child class based on selected type
+        if (type === 'maison') {
+            new_ad = new Maison(title, type, description, price, location, surface, garden, box, room);
+        } else if (type === 'appartement') {
+            new_ad = new Appartement(title, type, description, price, location, surface, ground, elevator, room, box);
+        } else if (type === 'terrain') {
+            new_ad = new Terrain(title, type, description, price, location, surface, buildable);
+        }
+
+        // Store new ad in localStorage
+        let ads = JSON.parse(localStorage.getItem('ads')) || [];
+        ads.push(new_ad);
+        localStorage.setItem('ads', JSON.stringify(ads));
+        window.location.replace("http://127.0.0.1:5500/index.html");
     });
 });
 
